b55e0e38a9af242df7a9527d97cde8f1844fca2c
Tim Weaver <inneralien@protonmail.com>
2022-03-22 11:05:18 -0600

    Add more unit tests.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
+use csv::{ReaderBuilder, StringRecord, Trim};
 use rust_decimal::prelude::*;
+use serde::Deserialize;
+use std::env;
+use std::error::Error;
+use std::ffi::OsString;
+use std::fs::File;
 use std::io;
 
 enum ClientErrors {
@@ -8,7 +14,7 @@ enum ClientErrors {
 
 // Client data
 // Assumption #1 - If an account is locked no future deposits/withdrawls are
-// allowed
+// allowed. There is no way to unlock an account once it is locked.
 #[derive(Default, Debug)]
 struct Client {
     available: Decimal,
@@ -50,12 +56,76 @@ impl Client {
     }
 }
 
-fn main() {}
+#[derive(Debug, Deserialize)]
+struct Transaction {
+    #[serde(rename = "type")]
+    trans: String,
+    client: u16,
+    tx: u32,
+    amount: Option<Decimal>,
+}
+
+/// Taken from <https://docs.rs/csv/latest/csv/tutorial/index.html#reading-csv>
+/// Returns the first positional argument sent to this process. If there are no
+/// positional arguments, then this returns an error.
+fn get_first_arg() -> Result<OsString, Box<dyn Error>> {
+    match env::args_os().nth(1) {
+        None => Err(From::from("expected 1 argument, e.g. transactions.csv")),
+        Some(file_path) => Ok(file_path),
+    }
+}
+
+fn read_csv(csv: impl io::Read) -> Result<(), Box<dyn Error>> {
+    let mut rdr = csv::ReaderBuilder::new().trim(Trim::All).from_reader(csv);
+    for result in rdr.deserialize() {
+        let transaction: Transaction = result?;
+        println!("{:#?}", transaction);
+    }
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    //    let filename = match get_first_arg() {
+    //        Ok(filename) => filename,
+    //        Err(error) => {
+    //            println!("{}", error);
+    //            process::exit(1);
+    //        }
+    //    };
+
+    let filename = get_first_arg()?;
+    read_csv(File::open(filename)?)?;
+
+    Ok(())
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use rust_decimal_macros::dec;
+
+    const DATA_SPACES: &'static str = "\
+type,       client,     tx,     amount
+deposit,         1,     1,         1.0
+deposit,         2,     2,         2.0
+deposit,         1,     3,         2.0
+withdrawal,      1,     4,         1.5
+withdrawal,      2,     5,         3.0
+";
+
+    const DATA_NO_SPACES: &'static str = "\
+type,client,tx,amount
+deposit,1,1,1.0
+deposit,2,2,2.0
+deposit,1,3,2.0
+withdrawal,1,4,1.5
+withdrawal,2,5,3.0
+";
+
+    const DATA_NO_HEADER: &'static str = "\
+deposit,1,1,1.0
+deposit,2,2,2.0
+";
 
     #[test]
     fn test_client_defaults() {
@@ -135,5 +205,26 @@ mod tests {
         assert_eq!(client.held, dec!(0));
         assert_eq!(client.total, amount);
         assert_eq!(client.locked, true);
+    }
+
+    #[test]
+    fn test_parse_csv_spaces() {
+        read_csv(DATA_SPACES.as_bytes()).unwrap();
+    }
+
+    #[test]
+    fn test_parse_csv_no_spaces() {
+        read_csv(DATA_NO_SPACES.as_bytes()).unwrap();
+    }
+
+    #[test]
+    fn test_parse_csv_no_header_fails() {
+        assert!(read_csv(DATA_NO_HEADER.as_bytes()).is_err());
+    }
+
+    #[test]
+    fn test_parse_csv_file() {
+        let filename = OsString::from_str("transactions.csv").unwrap();
+        assert!(read_csv(File::open(filename).unwrap()).is_ok());
     }
 }
